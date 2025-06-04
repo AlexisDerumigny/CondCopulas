55b16a786ae82bee471722d19202d7d2d6fd8dd7
AlexisDerumigny <AlexisDerumigny@users.noreply.github.com>
2025-06-04 17:35:11 +0200

    First working version of `measures_nonsimplifyingness_NP`

--- R/simpA.measures.R
+++ R/simpA.measures.R
@@ -8,6 +8,7 @@
 #' @param Z vector of \code{n} observations of the conditioning variable
 #'
 #'
+#' @export
 #'
 measures_nonsimplifyingness_NP <- function(
     X1, X2, Z, h,
@@ -16,8 +17,67 @@ measures_nonsimplifyingness_NP <- function(
     numericalInt = list(kind = "legendre", nGrid = 10))
 {
   .checkSame_nobs_X1X2Z(X1, X2, Z)
+  n = length(X1)
+
   .checkUnivX1X2Z(X1, X2, Z)
 
+  nGrid = numericalInt$nGrid
+  grid <- statmod::gauss.quad(n = nGrid, kind = numericalInt$kind)
+  # Change of range to be on [0,1]
+  grid$nodes <- grid$nodes * (1/2 - h) + 1/2
+  grid$weights <- grid$weights / 2
 
+  possible_measures = c("T1_CvM_Cs3", "T1_CvM_Cs4", "tilde_T0_CvM",
+                        "T1_KS_Cs3", "T1_KS_Cs4", "tilde_T0_KS")
+
+  if (length(measures) == 0){
+    stop(errorCondition(
+      message = paste0("'measures' should not be of length 0."),
+      class = "ZeroLengthError") )
+  } else if (length(measures) == 1 && measures == "all"){
+    measures = possible_measures
+  } else {
+    which_bad = which(!(measures %in% possible_measures))
+    if(length(which_bad) > 0){
+      stop(errorCondition(
+        message = paste0(
+          "Unknwon measure(s): ",
+          "'", paste0(measures[which_bad], collapse = "', '"), "'.",
+          "\n",
+          "Possible measures are: ",
+          "'", paste0(possible_measures, collapse = "', '"), "'."),
+        class = "UnknownMeasureNameError" ) )
+    }
+  }
+
+  result = data.frame(
+    name = measures,
+    value = NA_real_
+  )
+
+  env <- environment()
+
+  # Necessary to make the other functions work...
+  X3 = Z
+
+  for (i in 1:length(measures)){
+    result$value[i] <- switch(
+      measures[i],
+
+      "T1_CvM_Cs3" = {testStat_T1_CvM_Cs3(env); true_stat},
+
+      "T1_CvM_Cs4" = {testStat_T1_CvM_Cs4(env); true_stat},
+
+      "tilde_T0_CvM" = {testStat_tilde_T0_CvM(env); true_stat},
+
+      "T1_KS_Cs3" = {testStat_T1_KS_Cs3(env); true_stat},
+
+      "T1_KS_Cs4" = {testStat_T1_KS_Cs4(env); true_stat},
+
+      "tilde_T0_KS" = {testStat_tilde_T0_KS(env); true_stat}
+    )
+  }
+
+  return (result)
 }
 
--- tests/testthat/test-simpA.measures.R
+++ tests/testthat/test-simpA.measures.R
@@ -22,4 +22,39 @@ test_that("measures_nonsimplifyingness_NP does not work for non-valid input", {
     measures_nonsimplifyingness_NP(X1 = X1, X2 = X2, Z = Z, h = 1) },
     class = "WrongDimensionError")
 
+  Z = 2
+
+  # We now give invalid measures
+
+  expect_error({
+    measures_nonsimplifyingness_NP(X1 = X1, X2 = X2, Z = Z, h = 1, measures = c()) },
+    class = "ZeroLengthError")
+
+  expect_error({
+    measures_nonsimplifyingness_NP(X1 = X1, X2 = X2, Z = Z, h = 1, measures = "aaaa") },
+    class = "UnknownMeasureNameError")
+
 })
+
+
+test_that("measures_nonsimplifyingness_NP works for valid input", {
+
+  X1 = rnorm(100)
+  X2 = rnorm(100)
+  Z = rnorm(100)
+
+  # It works by default
+  expect_no_error({
+    measures_nonsimplifyingness_NP(X1 = X1, X2 = X2, Z = Z, h = 0.1) })
+
+  # It works for one of the measures
+  expect_no_error({
+    measures_nonsimplifyingness_NP(X1 = X1, X2 = X2, Z = Z, h = 1,
+                                   measures = "T1_CvM_Cs3") })
+
+  # It works for two measures
+  expect_no_error({
+    measures_nonsimplifyingness_NP(X1 = X1, X2 = X2, Z = Z, h = 1,
+                                   measures = c("T1_CvM_Cs3", "T1_CvM_Cs4")) })
+})
+
